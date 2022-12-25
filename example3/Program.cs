4365cfecca5f95d970de8c9e60bbc8b674a66813
Maxim <maxim@kryzh.ru>
2022-12-26 00:57:44 +0300

    Добавили генератор случайных чисел

--- example3/Program.cs
+++ example3/Program.cs
@@ -1,4 +1,7 @@
-﻿int a = 3;
-int b = 2;
+﻿//int a = new Random.Next(1,10);
+//int b = new Random.Next(1,10);
+Random rnd = new Random();//Создание объекта для генерации чисел 
+int a = rnd.Next(1, 10);
+int b = rnd.Next(1,10);
 Console.WriteLine(a + b);
 
